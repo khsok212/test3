c29508509452d6168a77159f3f0a0db04ddacd08
kang <khsok212@gmail.com>
2019-07-31 16:51:54 +0900

    수정

--- src/main/java/kr/or/ddit/servlet/HelloWorldServlet.java
+++ src/main/java/kr/or/ddit/servlet/HelloWorldServlet.java
@@ -14,6 +14,7 @@ public class HelloWorldServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		//수정
 		resp.setContentType("text/html;charset=utf-8");
 		PrintWriter pw = resp.getWriter();
 		Date today = new Date();
