6b2f9e809230940ab2f7bd3b6d37b0b5c6699ade
kang <khsok212@gmail.com>
2019-07-31 16:47:53 +0900

    test3 커밋

---
+++ src/main/java/kr/or/ddit/servlet/HelloWorldServlet.java
@@ -0,0 +1,40 @@
+package kr.or.ddit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = resp.getWriter();
+		Date today = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				
+		String formattedDate = sdf.format(today);
+		
+		
+		pw.write("<html>");
+		pw.write("	<head>");
+		pw.write("		<title>helloWorldServlet</title>");
+		pw.write("	</head>");
+		pw.write("	<body>");
+		pw.write("		<h1>hello, today : </h1> " + formattedDate + "(현재 날짜)");
+		pw.write("	</body>");
+		pw.write("</html>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
